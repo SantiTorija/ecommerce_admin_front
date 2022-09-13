d926faa20b902e27bcfbab4b25fd64a18c94be57
Guillermo <guillermogalain@gmail.com>
2022-09-13 19:31:17 -0300

    sidebars

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./Navbar";
-
+import Sidebar from "./Sidebar";
+import LayoutContent from "./LayoutContent";
 function Dashboard() {
 	return (
 		<div classNameName="sb-nav-fixed">
 			<Navbar />
+			<div id="layoutSidenav_nav">
+				<Sidebar />
+				<LayoutContent />
+			</div>
 		</div>
 	);
 }
