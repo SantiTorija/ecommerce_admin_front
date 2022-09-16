14fce657f8baad2d2711f86368a457553515e351
Guillermo <guillermogalain@gmail.com>
2022-09-16 17:16:07 -0300

    alert pesados presenta

--- src/components/DeleteAlert.jsx
+++ src/components/DeleteAlert.jsx
@@ -2,51 +2,51 @@ import Swal from "sweetalert2";
 import axios from "axios";
 
 export async function handleDeleteWine(id) {
-  async function productDelete() {
-    await axios({
-      method: "DELETE",
-      url: `http://localhost:8000/wines/${id}`,
-    });
-    return;
-  }
-  return Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      productDelete();
-      Swal.fire("Deleted!", "Your file has been deleted.", "success");
-      this.forceUpdate();
-    }
-  });
+	async function productDelete() {
+		await axios({
+			method: "DELETE",
+			url: `http://localhost:8000/wines/${id}`,
+		});
+		return;
+	}
+	return Swal.fire({
+		title: "Are you sure?",
+		text: "You won't be able to revert this!",
+		icon: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#3085d6",
+		cancelButtonColor: "#d33",
+		confirmButtonText: "Yes, delete it!",
+	}).then((result) => {
+		if (result.isConfirmed) {
+			productDelete();
+			Swal.fire("Deleted!", "Your file has been deleted.", "success");
+			this.forceUpdate();
+		}
+	});
 }
 
 export async function handleDeleteAdmin(id) {
-  async function adminDelete() {
-    await axios({
-      method: "DELETE",
-      url: `http://localhost:8000/admin/${id}`,
-    });
-    return;
-  }
-  return Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      adminDelete();
-      Swal.fire("Deleted!", "Your file has been deleted.", "success");
-      this.forceUpdate();
-    }
-  });
+	async function adminDelete() {
+		await axios({
+			method: "DELETE",
+			url: `http://localhost:8000/admin/${id}`,
+		});
+		return;
+	}
+	Swal.fire({
+		title: "Are you sure?",
+		text: "You won't be able to revert this!",
+		icon: "warning",
+		showCancelButton: true,
+		confirmButtonColor: "#3085d6",
+		cancelButtonColor: "#d33",
+		confirmButtonText: "Yes, delete it!",
+	}).then((result) => {
+		if (result.isConfirmed) {
+			adminDelete();
+			Swal.fire("Deleted!", "Your file has been deleted.", "success");
+			this.forceUpdate();
+		}
+	});
 }
