3f3a8a147149dca4e4dbf4de7e6a8a9b402de03d
SantiTorija <torija.santiago@gmail.com>
2022-09-19 14:12:03 -0300

    peoduct form mejorada

--- src/components/NewProductForm.jsx
+++ src/components/NewProductForm.jsx
@@ -9,12 +9,12 @@ function CreateProductForm() {
   const [name, setName] = useState(null);
   const [picture, setPicture] = useState(null);
   const [variety, setVariety] = useState(null);
+  const [wineType, setWineType] = useState(null);
   const [country, setCountry] = useState(null);
   const [region, setRegion] = useState(null);
   const [harvest, setHarvest] = useState(null);
   const [cellar, setCellar] = useState(null);
   const [capacity, setCapacity] = useState(null);
-  const [tastingNote, setTastingNote] = useState(null);
   const [description, setDescription] = useState(null);
   const [price, setPrice] = useState(null);
   const [stock, setStock] = useState(null);
@@ -25,7 +25,6 @@ function CreateProductForm() {
     const data = new FormData();
     data.append("file", files[0]);
     data.append("upload_preset", "ecommerce");
-    /*  setLoading(true); */
     const res = await fetch(
       "https://api.cloudinary.com/v1_1/dazcipdps/image/upload",
       {
@@ -46,16 +45,16 @@ function CreateProductForm() {
           name: name,
           picture: picture,
           variety: variety,
+          type: wineType,
           country: country,
           region: region,
           harvest: harvest,
           cellar: cellar,
           capacity: capacity,
-          tastingNote: tastingNote,
           description: description,
           price: price,
           stock: stock,
-          /* highlighted*/
+          highlighted: highlighted,
         },
       });
       console.log();
@@ -76,93 +75,111 @@ function CreateProductForm() {
       </div>
 
       <Form
+        style={{ maxWidth: "50rem" }}
         onSubmit={() => {
           createProduct();
           navigate("/Productos");
         }}
       >
-        <Form.Group className="mb-3">
-          <Form.Label>Producto</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Nombre del vino"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
+        <div className="d-flex mb-4">
+          <Form.Group className=" me-5 w-100">
+            <Form.Label>Producto</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Nombre del vino"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
 
-        <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>Seleccionar Archivo</Form.Label>
-          <Form.Control type="file" onChange={upLoadImage} />
-        </Form.Group>
+          <Form.Group controlId="formFile" className=" w-100">
+            <Form.Label>Seleccionar foto</Form.Label>
+            <Form.Control type="file" onChange={upLoadImage} />
+          </Form.Group>
+        </div>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Variedad</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Variedad"
-            onChange={(e) => setVariety(e.target.value)}
-          />
-        </Form.Group>
+        <div className="d-flex mb-4">
+          <Form.Group className=" me-5 w-100">
+            <Form.Label>Variedad</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Variedad"
+              onChange={(e) => setVariety(e.target.value)}
+            />
+          </Form.Group>
 
-        {/* 		<Form.Group className="mb-3">
-				<Form.Label>Tipo</Form.Label>
-				<Form.Control type="text" defaultValue={wine.type} />
-			</Form.Group> */}
+          <div className=" w-100">
+            <Form.Label>Seleccione tipo de vino</Form.Label>
+            <Form.Select
+              onChange={(e) => setWineType(e.target.value)}
+              aria-label="Default select example"
+            >
+              <option>...</option>
+              <option value="1">Tinto</option>
+              <option value="2">Blanco</option>
+              <option value="3">Rose</option>
+              <option value="3">Espumante</option>
+            </Form.Select>
+          </div>
+        </div>
+        <div className="d-flex mb-4">
+          <Form.Group className="w-100 me-5">
+            <Form.Label>País</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Pais de procedencia"
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>País</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Pais de procedencia"
-            onChange={(e) => setCountry(e.target.value)}
-          />
-        </Form.Group>
+          <Form.Group className="w-100">
+            <Form.Label>Región</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="región de procedencia"
+              onChange={(e) => setRegion(e.target.value)}
+            />
+          </Form.Group>
+        </div>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Región</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="región de procedencia"
-            onChange={(e) => setRegion(e.target.value)}
-          />
-        </Form.Group>
+        <div className="d-flex mb-4">
+          <Form.Group className="me-5 w-100">
+            <Form.Label>Cosecha</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Año de cosecha"
+              onChange={(e) => setHarvest(e.target.value)}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Cosecha</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Año de cosecha"
-            onChange={(e) => setHarvest(e.target.value)}
-          />
-        </Form.Group>
+          <Form.Group className="w-100">
+            <Form.Label>Bodega</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Bodega"
+              onChange={(e) => setCellar(e.target.value)}
+            />
+          </Form.Group>
+        </div>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Bodega</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Bodega"
-            onChange={(e) => setCellar(e.target.value)}
-          />
-        </Form.Group>
+        <div className="d-flex mb-4">
+          <Form.Group className="w-100 me-5">
+            <Form.Label>Capacidad</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="capcidad"
+              onChange={(e) => setCapacity(e.target.value)}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Capacidad</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="capcidad"
-            onChange={(e) => setCapacity(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Nota de Cata</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows="4"
-            placeholder="Nota de cata"
-            onChange={(e) => setTastingNote(e.target.value)}
-          />
-        </Form.Group>
+          <Form.Group className="w-100 ">
+            <Form.Label>Precio</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Precio en dólares"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </Form.Group>
+        </div>
 
         <Form.Group className="mb-3">
           <Form.Label>Descripción</Form.Label>
@@ -174,49 +191,40 @@ function CreateProductForm() {
           />
         </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Precio</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Precio en dólares"
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </Form.Group>
+        <div className="d-flex mb-4">
+          <Form.Group className="mb-3 me-5 w-100">
+            <Form.Label>Stock</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Cantidad en inventario"
+              onChange={(e) => setStock(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="me-5 w-100">
+            <Form.Label>Destacado</Form.Label>
+            <Form.Check
+              type="radio"
+              label="si"
+              onClick={() => setHighlighted(true)}
+              name="highlighted"
+              id="default-radio"
+            />
+            <Form.Check
+              type="radio"
+              label="no"
+              onClick={() => setHighlighted(false)}
+              name="highlighted"
+              id="default-radio"
+            />
+          </Form.Group>
+        </div>
 
-        <Form.Group className="mb-3">
-          <Form.Label>Stock</Form.Label>
-          <Form.Control
-            type="number"
-            placeholder="Cantidad en inventario"
-            onChange={(e) => setStock(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Destacado</Form.Label>
-          <Form.Check
-            type="radio"
-            label="si"
-            onChange={() => setHighlighted(true)}
-            name="highlighted"
-            id="default-radio"
-          />
-          <Form.Check
-            type="radio"
-            label="no"
-            onChange={() => setHighlighted(false)}
-            name="highlighted"
-            id="default-radio"
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
+        <Button className="mb-5" variant="primary" type="submit">
           Submit
         </Button>
       </Form>
     </div>
   );
 }
-/* } */
 
 export default CreateProductForm;
