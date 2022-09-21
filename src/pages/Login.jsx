bd8edf086ab5f6c76ada997a8466ca4a6c922765
alankatz3793 <alankatz2000@gmail.com>
2022-09-20 22:37:46 -0300

    datos iniciales

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -56,6 +56,7 @@ function Login() {
           <Form.Control
             onChange={(e) => setEmail(e.target.value)}
             type="email"
+            defaultValue={email}
             placeholder="Ingrese email"
           />
         </Form.Group>
@@ -65,6 +66,7 @@ function Login() {
             onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Password"
+            defaultValue={password}
           />
         </Form.Group>
         <Button className="w-100" variant="primary" type="submit">
